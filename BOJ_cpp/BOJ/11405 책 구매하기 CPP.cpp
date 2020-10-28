8875a5904cd225fadc588983d1e3f636e33703ce
leeyongwoo93 <rmstjd23@gmail.com>
2020-10-28 23:29:26 +0900

    testCommit
    beeeee

---
+++ BOJ_cpp/BOJ/11405 책 구매하기 CPP.cpp
@@ -0,0 +1,20 @@
+#include "control.h"
+//
+//	control.h 에서 확인하고자 하는 문제 번호를 선언해주어야 함
+//
+
+#ifdef _11405_
+// https://www.acmicpc.net/problem/11405
+
+
+
+int main()
+{
+
+
+
+	return 0;
+}
+
+
+#endif
