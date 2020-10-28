4b1160c6f85b44f55a9c158c66d2df847ccbe737
leeyongwoo93 <rmstjd23@gmail.com>
2020-10-28 17:41:17 +0900

    1395 fix
    vector to tree

--- BOJ_cpp/BOJ/1395 스위치 CPP.cpp
+++ BOJ_cpp/BOJ/1395 스위치 CPP.cpp
@@ -8,29 +8,29 @@
 
 #include <iostream>
 #include <vector>
-#include <math.h>
 
 using namespace std;
 
 typedef struct node{
 	int on;
+	int off;
 	bool isReverse;
+
+	node* left;
+	node* right;
 }node;
 
-void reversUpdate(int treeLeftBoundary, int treeRightBoundary, int root, vector<int>& tree, vector<bool>& reverse);
-int setReverse(int treeLeftBoundary, int treeRightBoundary, int rangeLeft, int rangeRight, int root, vector<int>& tree, vector<bool>& reverse);
-int getOnCount(int treeLeftBoundary, int treeRightBoundary, int rangeLeft, int rangeRight, int root, vector<int>& tree, vector<bool>& reverse);
+node* getSegmentTree(int start, int end);
+void setReverse(int treeLeftBoundary, int treeRightBoundary, int rangeLeft, int rangeRight, node* root);
+int getOnCount(int treeLeftBoundary, int treeRightBoundary, int rangeLeft, int rangeRight, node* root);
 
 int main()
 {
 	int switchCount;
 	int taskCount;
 
-	scanf("%d", &switchCount);
-	int treeSize = ceil(log2(switchCount)) + 1;
-
-	vector<int> tree(pow(2, treeSize), 0);
-	vector<bool> reverse(pow(2, treeSize), false);
+	scanf("%d",  &switchCount);
+	node* root = getSegmentTree(1, switchCount);
 
 	scanf("%d", &taskCount);
 	for (int i = 0; i < taskCount; i++)
@@ -42,69 +42,151 @@ int main()
 
 		if (tasktype == 0)
 		{
-			setReverse(1, switchCount, start, end, 1, tree, reverse);
+			setReverse(1, switchCount, start, end, root);
 		}
 		else
 		{
-			printf("%d\n", getOnCount(1, switchCount, start, end, 1, tree, reverse));
+			printf("%d\n",getOnCount(1, switchCount, start, end, root));
 		}
 	}
-	
+
 	return 0;
 }
 
 
-void reversUpdate(int treeLeftBoundary, int treeRightBoundary, int root, vector<int>& tree, vector<bool>& reverse)
+node* getSegmentTree(int start, int end)
 {
-	if (reverse[root] == true)
+	node* ret = new node();
+
+	if (start == end)
 	{
-		tree[root] = (treeRightBoundary - treeLeftBoundary + 1) - tree[root];
+		ret->left = NULL;
+		ret->right = NULL;
+		ret->on = 0;
+		ret->off = 1;
+		ret->isReverse = false;
+
+		return ret;
+	}
+
+	ret->left = getSegmentTree(start, (start + end) / 2);
+	ret->right = getSegmentTree((start + end) / 2 + 1, end);
+	ret->on = ret->left->on + ret->right->on;
+	ret->off = ret->left->off + ret->right->off;
+	ret->isReverse = false;
+
+	return ret;
+}
+
+void setReverse(int treeLeftBoundary, int treeRightBoundary, int rangeLeft, int rangeRight, node* root)
+{
+	if (root->isReverse == true)
+	{
+		int temp = root->off;
+		root->off = root->on;
+		root->on = temp;
 
 		if (treeLeftBoundary != treeRightBoundary)
 		{
-			reverse[root * 2] = !reverse[root * 2];
-			reverse[root * 2 + 1] = !reverse[root * 2 + 1];
+			if (root->left != NULL)
+				root->left->isReverse = root->left->isReverse ? false : true;
+			if (root->right != NULL)
+				root->right->isReverse = root->right->isReverse ? false : true;
 		}
 
-		reverse[root] = false;
+		root->isReverse = false;
 	}
-}
 
-int setReverse(int treeLeftBoundary, int treeRightBoundary, int rangeLeft, int rangeRight, int root, vector<int>& tree, vector<bool>& reverse)
-{
-	reversUpdate(treeLeftBoundary, treeRightBoundary, root, tree, reverse);
+
 
 	if (rangeLeft > treeRightBoundary || rangeRight < treeLeftBoundary)
-		return tree[root];
+		return;
 
 	if (rangeLeft <= treeLeftBoundary && rangeRight >= treeRightBoundary)
 	{
-		tree[root] = (treeRightBoundary - treeLeftBoundary + 1) - tree[root];
+		int temp = root->off;
+		root->off = root->on;
+		root->on = temp;
 
 		if (treeLeftBoundary != treeRightBoundary)
 		{
-			reverse[root * 2] = !reverse[root * 2];
-			reverse[root * 2 + 1] = !reverse[root * 2 + 1];
+			if (root->left != NULL)
+				root->left->isReverse = root->left->isReverse ? false : true;
+			if (root->right != NULL)
+				root->right->isReverse = root->right->isReverse ? false : true;
 		}
 
-		return tree[root];
+		return;
 	}
 
-	return tree[root] = setReverse(treeLeftBoundary, (treeLeftBoundary + treeRightBoundary) / 2, rangeLeft, rangeRight, root * 2, tree, reverse) + setReverse((treeLeftBoundary + treeRightBoundary) / 2 + 1, treeRightBoundary, rangeLeft, rangeRight, root * 2 + 1, tree, reverse);
+	if (rangeLeft <= (treeLeftBoundary + treeRightBoundary) / 2)
+		setReverse(treeLeftBoundary, (treeLeftBoundary + treeRightBoundary) / 2, rangeLeft, rangeRight, root->left);
+	if (rangeRight > (treeLeftBoundary + treeRightBoundary) / 2)
+		setReverse((treeLeftBoundary + treeRightBoundary) / 2 + 1, treeRightBoundary, rangeLeft, rangeRight, root->right);
+
+	root->off = 0;
+	root->on = 0;
+
+	if (root->left != NULL)
+	{
+		if (root->left->isReverse)
+		{
+			root->off += root->left->on;
+			root->on += root->left->off;
+		}
+		else
+		{
+			root->off += root->left->off;
+			root->on += root->left->on;
+		}
+	}
+	if (root->right != NULL)
+	{
+		if (root->right->isReverse)
+		{
+			root->off += root->right->on;
+			root->on += root->right->off;
+		}
+		else
+		{
+			root->off += root->right->off;
+			root->on += root->right->on;
+		}
+	}
 }
 
 
-int getOnCount(int treeLeftBoundary, int treeRightBoundary, int rangeLeft, int rangeRight, int root, vector<int>& tree, vector<bool>& reverse)
+int getOnCount(int treeLeftBoundary, int treeRightBoundary, int rangeLeft, int rangeRight, node* root)
 {
-	reversUpdate(treeLeftBoundary, treeRightBoundary, root, tree, reverse);
+	if (root->isReverse == true)
+	{
+		int temp = root->off;
+		root->off = root->on;
+		root->on = temp;
+
+		if (treeLeftBoundary != treeRightBoundary)
+		{
+			if (root->left != NULL)
+				root->left->isReverse = root->left->isReverse ? false : true;
+			if (root->right != NULL)
+				root->right->isReverse = root->right->isReverse ? false : true;
+		}
+
+		root->isReverse = false;
+	}
 
 	if (rangeLeft > treeRightBoundary || rangeRight < treeLeftBoundary)
 		return 0;
 
 	if (rangeLeft <= treeLeftBoundary && rangeRight >= treeRightBoundary)
-		return tree[root];
-	
-	return getOnCount(treeLeftBoundary, (treeLeftBoundary + treeRightBoundary) / 2, rangeLeft, rangeRight, root * 2, tree, reverse) + getOnCount((treeLeftBoundary + treeRightBoundary) / 2 + 1, treeRightBoundary, rangeLeft, rangeRight, root * 2 + 1, tree, reverse);
+	{
+		if (root->isReverse)
+			return root->off;
+
+		return root->on;
+	}
+
+	return getOnCount(treeLeftBoundary, (treeLeftBoundary + treeRightBoundary) / 2, rangeLeft, rangeRight, root->left) + getOnCount((treeLeftBoundary + treeRightBoundary) / 2 + 1, treeRightBoundary, rangeLeft, rangeRight, root->right);
 }
 
 
