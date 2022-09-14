22b47899eec73e0e79089c45f6d1839b8917d1ff
YoungWoo93 <rmstjd23@gmail.com>
2022-09-14 12:17:54 +0900

    0914

--- BOJ/cpp/12850 본대 산책2/12850.cpp
+++ BOJ/cpp/12850 본대 산책2/12850.cpp
@@ -17,41 +17,62 @@
 #include <limits.h>
 
 using namespace std;
-vector<vector<bool>> map = {
-	{false, true, true, false, false, false, false},
-	{true, false, true, true, true, false, false},
-	{true, true, false, true, false, false, false},
-	{false, true, false, false, true, true, false},
-	{false, true, true, true, false, false, true},
-	{false, false, false, true, false, false, true},
-	{false, false, false, false, true, true, false}
+vector<vector<unsigned long long int>> map = {
+	{0, 1, 0, 1, 0, 0, 0, 0},
+	{1, 0, 1, 1, 0, 0, 0, 0},
+	{0, 1, 0, 1, 1, 1, 0, 0},
+	{1, 1, 1, 0, 0, 1, 0, 0},
+	{0, 0, 1, 0, 0, 1, 1, 0},
+	{0, 0, 1, 1, 1, 0, 0, 1},
+	{0, 0, 0, 0, 1, 0, 0, 1},
+	{0, 0, 0, 0, 0, 1, 1, 0}
 };
 
-vector<vector<vector<int>>> dpTable;
-
-void process(int step)
+vector<vector<unsigned long long int>> process(vector<vector<unsigned long long int>>& map1, vector<vector<unsigned long long int>>& map2)
 {
-	
+	vector<vector<unsigned long long int>> ret(map.size(), vector<unsigned long long int>(map.front().size(), 0));
+	for (int y = 0; y < map.size(); y++)
+	{
+		for (int x = 0; x < map.front().size(); x++)
+		{
+			for (int i = 0; i < map.size(); i++)
+			{
+				ret[y][x] += map1[y][i] * map2[i][x];
+				ret[y][x] = ret[y][x] % 1000000007;
+			}
+		}
+	}
+
+	return ret;
 }
+
 int main()
 {
 	int n;
 	cin >> n;
 
-	int answer = 0;
-	process(n);
+	auto matrix = map;
 
+	vector<vector<unsigned long long int>> answer(map.size(), vector<unsigned long long int>(map.front().size(), 0));
+	for (int i = 0; i < 8; i++) 
+		answer[i][i] = 1; 
+	
 
-	answer += dpTable[n - 1][0][0];
-	answer = answer % 1000000007;
-	answer += dpTable[n - 1][1][0];
-	answer = answer % 1000000007;
-	answer += dpTable[n - 1][0][1];
-	answer = answer % 1000000007;
-	answer += dpTable[n - 1][1][1];
-	answer = answer % 1000000007;
+	while (n > 0)
+	{
+		if (n % 2 == 0)
+		{
+			matrix = process(matrix, matrix);
+			n /= 2;
+		}
+		else
+		{
+			answer = process(answer, matrix);
+			n -= 1;
+		}
+	}
 
-	cout << answer << endl;
+	cout << answer[0][0] << endl;
 	
 }
 #endif
